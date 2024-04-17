d76c4a737f3ff85bc2e133425eb46bd36cf00bec
RAHUL KUMAR <rahul.kumar3@batterysmart.in>
2024-04-17 11:23:30 +0530

    Done with basic setup with folder structure

---
+++ src/components/Flavanoids/Flavanoids.tsx
@@ -0,0 +1,31 @@
+import { Table } from '@mantine/core';
+import React from 'react';
+import { data } from '../../constants/data';
+
+const Flavanoids: React.FC = () => {
+
+// const rows = data.map((element) => (
+//     <Table.Tr key={data.}>
+//       <Table.Td>{element.position}</Table.Td>
+//       <Table.Td>{element.name}</Table.Td>
+//       <Table.Td>{element.symbol}</Table.Td>
+//       <Table.Td>{element.mass}</Table.Td>
+//     </Table.Tr>
+//   ));
+
+  return (
+    <Table>
+      <Table.Thead>
+        <Table.Tr>
+          <Table.Th>Element position</Table.Th>
+          <Table.Th>Element name</Table.Th>
+          <Table.Th>Symbol</Table.Th>
+          <Table.Th>Atomic mass</Table.Th>
+        </Table.Tr>
+      </Table.Thead>
+      {/* <Table.Tbody>{rows}</Table.Tbody> */}
+    </Table>
+  );
+};
+
+export default Flavanoids;
---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import Flavanoids from '../components/Flavanoids/Flavanoids'
+
+export default function Dashboard() {
+  return (
+    <div>
+        <Flavanoids/>
+
+    </div>
+  )
+}
